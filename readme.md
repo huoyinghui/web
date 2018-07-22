ff9a4b1271daa1f1c839b8c53b51850e1ceb5a87
huoyinghui <2285020853@qq.com>
2018-07-22 11:05:14 +0800

    init ok

---
+++ readme.md
@@ -0,0 +1,13 @@
+apps: app
+
+media: 上传
+
+
+
+##
+```
+    from message import views
+ModuleNotFoundError: No module named 'message'
+
+setting: 设置路径
+```
--- web/settings.py
+++ web/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -128,3 +131,21 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
+#
+# EMAIL_HOST = "smtp.sina.com"
+# EMAIL_PORT = 25
+# EMAIL_HOST_USER = "projectsedu@sina.com"
+# EMAIL_HOST_PASSWORD = "admin123"
+# EMAIL_USE_TLS= False
+# EMAIL_FROM = "projectsedu@sina.com"
+#
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
