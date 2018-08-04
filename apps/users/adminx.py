3fc607a97d391cceac5f5bd699e10214c4219636
huoyinghui <2285020853@qq.com>
2018-08-05 00:39:07 +0800

    update: xadmin 建立第一个标准管理模型

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -6,27 +6,45 @@ from xadmin import views
 from .models import EmailVerifyRecord, Banner
 
 # Register your models here.
-
-
-# class UserProfileAdmin(object):
-#     """
-#
-#     """
-#
-#     pass
-
 # 使用默认的
 # xadmin.site.unregister(UserProfile)
 # xadmin.site.register(UserProfile, UserProfileAdmin)
 
-class EmailVerifyRecordAdmin(object):
-    pass
 
-# class BaseSetting(object):
-#     enable_thems = True
-#     use_bootswatch = True
-#
+class EmailVerifyRecordAdmin(object):
+    """
+    默认模型显示模式:
+
+    1.search_fields:    #搜索范围
+    2.list_display:     #列表显示
+    3.list_filter:      #列表过滤
+    4.ordering:         #默认排序,'-':倒序,从大到小
+    5.readonly_fields   # 直接编辑
+    6.refresh_times     # 刷新秒数
+    7.list_editable     # 直接编辑
+    """
+    list_display = ['id', 'code', 'email', 'send_type', 'send_time']
+    search_fields = ['code', 'email', 'send_type']
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+    ordering = ['-id']
+    readonly_fields = ['email', 'send_type', 'send_time']
+    refresh_times = [3, 5]
+    list_editable = ['code']
+
+
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+
+
+class BaseSetting(object):
+    enable_thems = True
+    use_bootswatch = True
+
 
 # 将Xadmin全局管理器与我们的view绑定注册。
-# xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
+
